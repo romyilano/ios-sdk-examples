abb9dcc4325b3d902137a4bb6125423c4244e27e
jmkiley <jordan.kiley@mapbox.com>
2017-11-03 19:03:00 -0700

    Added to list, started Swift example

--- Examples/Examples.m
+++ Examples/Examples.m
@@ -35,6 +35,7 @@
         MBXExample3DExtrusions,
         MBXExampleLight,
         MBXExampleFillPattern,
+        MBXExampleMapSnapshotter,
         MBXExampleOfflinePack,
         MBXExamplePointConversion,
         MBXExamplePointHotspot,
